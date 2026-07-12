8839a5ea1722d409a1314763ebe4701a1f47180b
Dadeathmag <mohammedramshad246@gmail.com>
2026-07-12 14:39:54 +0530

    editted home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,41 @@
 import { useState } from "react";
+import "./Home.css";
 
-function Home() {
+export default function Home() {
+  const [roomCode, setRoomCode] = useState("");
+
+  const handleCreateRoom = () => {
+    console.log("Create Room");
+  };
+
+  const handleJoinRoom = () => {
+    if (!roomCode.trim()) return;
+
+    console.log("Joining:", roomCode);
+  };
+
   return (
-    <>
-      <p>Home</p>
-    </>
+    <div className="home">
+      <div className="card">
+        <h1>Welcome</h1>
+
+        <button className="create-btn" onClick={handleCreateRoom}>
+          Create Room
+        </button>
+
+        <div className="join-container">
+          <input
+            type="text"
+            placeholder="Enter Room Code"
+            value={roomCode}
+            onChange={(e) => setRoomCode(e.target.value)}
+          />
+
+          <button className="join-btn" onClick={handleJoinRoom}>
+            Join Room
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default Home;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,7 +1,44 @@
+import './Room.css'
+
 function Room(){
     return(
         <>
-            <p>Room</p>
+            <div className="Room">
+                <div className="video-container">
+                    <div className="video-placeholder">
+                    🎬 VIDEO PLAYER
+                    </div>
+
+                    <div className="chat-overlay">
+                    <div className="chat-message">
+                        <strong>Alice:</strong> 😂
+                    </div>
+
+                    <div className="chat-message">
+                        <strong>Bob:</strong> Nice!
+                    </div>
+                    </div>
+                </div>
+
+                <div className="controls">
+                    <div className="left-controls">
+                    <button>▶</button>
+                    <button>⏸</button>
+                    <button>⏩</button>
+                    </div>
+
+                    <div className="time">
+                    00:21 / 01:43:12
+                    </div>
+
+                    <div className="right-controls">
+                    <button>🔊</button>
+                    <button>👥</button>
+                    <button>🎤</button>
+                    <button>💬</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
